ae192cc8d62103b0db76f27de0f2b119b8e80897
Shobair Abidi <shobair.abidi@gmail.com>
2021-06-20 14:40:49 -0400

    refactored the javascript to utilize more functions and nested object var

--- HW03/assets/js/script.js
+++ HW03/assets/js/script.js
@@ -10,59 +10,100 @@ function writePassword() {
 //My Function for Password Generation
 function generatePassword() {
   //Defining constant vars 
-  var pwLengthMax = 128;
-  var pwLengthMin = 8;
   var alphabet = "abcdefghijklmnopqrstuvwxyz";
-  var pwCharAvailable = {
-    lowerChars: [false, alphabet],
-    upperChars: [false, alphabet.toUpperCase()],
-    numbrChars: [false, "0123456789"],
-    speclChars: [false, " !\"#$%&'()*+,-./:;<>=?@[]^\\_`{}|~"]
-  };
   //Defining input vars & counters
+  var pwCharAvailable = {
+    lowerChars: {
+      name: "Lowercase",
+      isIncluded: false,
+      values: alphabet},
+    upperChars: {
+      name: "Uppercase",
+      isIncluded: false,
+      values: alphabet.toUpperCase()},
+    numbrChars: {
+      name: "Numeric",
+      isIncluded: false,
+      values: "0123456789"},
+    speclChars: {
+      name: "Special",
+      isIncluded: false,
+      values: " !\"#$%&'()*+,-./:;<>=?@[]^\\_`{}|~"},
+  };
   var pwLength = 0;
+  var chartypeEscape = true;
   var pwCharArray = [];
   var pwArray = [];
   var j = 0;
   //We want the ability to re-prompt the user for valid inputs if they don't provide them on the first pass without having to reset completely
   do {
-    //alert the user to the requirement and prompt them for their input
-    this.alert("The password must be a numeric value between 8 and 128 characters.");
     pwLength = parseInt(this.prompt("How long would you like the password to be?"));
     //validate password requirement is met before proceeding
-  } while ((isNaN(pwLength)) || (pwLength < pwLengthMin || pwLength > pwLengthMax));
+  } while (validatePwLength(pwLength));
   do {
-    //alert the user to the requirement and prompt them for their input
-    this.alert("You must select at least one valid Character Type.");
-    pwCharAvailable.lowerChars[0] = this.confirm("Would you like to use Lowercase Characters?");
-    pwCharAvailable.upperChars[0] = this.confirm("Would you like to use Uppercase Characters?");
-    pwCharAvailable.numbrChars[0] = this.confirm("Would you like to use Numeric Characters?");
-    pwCharAvailable.speclChars[0] = this.confirm("Would you like to use Special Characters?");
-    //validate password requirement is met before proceeding
-  } while (!pwCharAvailable.lowerChars[0] && !pwCharAvailable.upperChars[0] && !pwCharAvailable.numbrChars[0] && !pwCharAvailable.speclChars[0]);
-  //If a character type is true, add it to the full pool of possible characters for out password
-  for (i in pwCharAvailable) {
-    if (pwCharAvailable[i][0]) {
-      var temparray = Array.from(pwCharAvailable[i][1]);
-      pwCharArray = pwCharArray.concat(temparray);
-      //manually setting one character of each true type to guarantee the character requirements are met
-      pwArray = pwArray.concat(temparray[Math.floor(Math.random() * temparray.length)]);
-      pwLength--;
+    for (chartype in pwCharAvailable) {
+      pwCharAvailable[chartype].isIncluded = this.confirm(`Would you like to use ${pwCharAvailable[chartype].name} Characters?`);
+      //If a character type is true, add it to the full pool of possible characters for out password
+      if (pwCharAvailable[chartype].isIncluded) {
+        chartypeEscape = false;
+        var temparray = Array.from(pwCharAvailable[chartype].values);
+        temparray = shuffleMe(temparray);
+        pwCharArray = pwCharArray.concat(temparray);
+        //manually setting one character of each true type to guarantee the character requirements are met
+        pwArray = addToPassword(pwArray, temparray);
+        pwLength--;
+      }
     }
-  }
+    //validate password requirement is met before proceeding
+  } while (validateCharInputs(chartypeEscape));
   //for the remaining character slots not manually set we want to assign any random character from the full pool
   for (i=0; i < pwLength; i++) {
-    pwArray = pwArray.concat(pwCharArray[Math.floor(Math.random() * pwCharArray.length)])
+    pwArray = addToPassword(pwArray, pwCharArray);
   }
-  //Implementing a shuffle function for the Character Array to remove the the setting for the character guarantee
-  for (i=0; i < pwArray.length - 2; i++) {
-    var j = Math.floor((Math.random() * (pwArray.length - i)) + i);
-    var shuffle1 = pwArray[i];
-    var shuffle2 = pwArray[j];
-    pwArray[i] = shuffle2;
-    pwArray[j] = shuffle1;
-  }
+  pwArray = shuffleMe(pwArray);
   return pwArray.join("");
+}
+// function for validating the password length
+function validatePwLength(pwLength) {
+  var pwLengthMax = 128;
+  var pwLengthMin = 8;
+  if (isNaN(pwLength)) {
+    //alert the user to the requirement and prompt them for their input
+    this.alert("The password length must be a numeric value.");
+    return true;
+  } else if (pwLength < pwLengthMin || pwLength > pwLengthMax) {
+    //alert the user to the requirement and prompt them for their input
+    this.alert("The password must be between 8 and 128 characters.");
+    return true;
+  } else {
+    return false;
+  }
+}
+// function for validating the character type inputs
+function validateCharInputs(boolEscape) {
+  if (boolEscape) {
+    //alert the user to the requirement and prompt them for their input
+    this.alert("You must select at least one valid Character Type.");
+    return true;
+  } else {
+    return false;
+  }
+}
+// function to add random char to pw from pool
+function addToPassword(varPW, varPool) {
+  varPW = varPW.concat(varPool[Math.floor(Math.random() * varPool.length)]);
+  return varPW;
+}
+// function to shuffle array
+function shuffleMe(inputArray) {
+  for (i=0; i < inputArray.length - 2; i++) {
+    var j = Math.floor((Math.random() * (inputArray.length - i)) + i);
+    var shuffle1 = inputArray[i];
+    var shuffle2 = inputArray[j];
+    inputArray[i] = shuffle2;
+    inputArray[j] = shuffle1;
+  }
+  return inputArray;
 }
 //#####################################################################################################
 // Add event listener to generate button
